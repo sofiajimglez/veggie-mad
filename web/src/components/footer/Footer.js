9f283f3a6326dbdb3e65949a182cb3ad1f5b846c
Sofía J <sofiajimglez@gmail.com>
2023-05-04 15:09:53 +0200

    fix: footer position

--- web/src/components/footer/Footer.js
+++ web/src/components/footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="p-2 fixed-bottom w-100 bottom-0" data-bs-theme="dark">
+    <footer className="p-2 w-100 bottom-0" data-bs-theme="dark">
       <div className="container text-center">
         <span className="ms-1">veggieMAD 🐻🥦</span>
       </div>
