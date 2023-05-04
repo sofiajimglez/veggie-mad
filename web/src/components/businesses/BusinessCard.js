fb0d139aca33f1e8ff276b6c484e1b4c4ae1cd0f
Sofía J <sofiajimglez@gmail.com>
2023-05-04 16:44:40 +0200

    feat: redesign business item in business list

--- web/src/components/businesses/BusinessCard.js
+++ web/src/components/businesses/BusinessCard.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Businesses.css';
 
 export default function BusinessCard({ business, styles }) {
   return (
     <div className={styles}>
-      <div className='card'>
+      <div className='card business-card h-100 justify-content-end'>
         <img src={business.imageUrl} className="card-img-top" alt={business.name} />
         <div className="card-body">
           <h5 className="card-title">{business.name}</h5>
-          <p className="card-text">{business.category}</p>
-          <Link to={`/explora-madrid/${business.id}`} className="btn btn-primary w-100">Ver</Link>
+          <p className="card-text mb-3">{business.category}</p>
+          {business?.tags.map(tag => <span className='business-card-tag me-2 pb-2 align-text-middle'>#{tag}</span>)}
+          <Link to={`/explora-madrid/${business.id}`} className="btn btn-main w-100 mt-3">Ver </Link>
         </div>
       </div>
     </div>
